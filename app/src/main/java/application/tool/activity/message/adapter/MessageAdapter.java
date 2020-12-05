98839378236fff528e1b2a7f9d99b96ab1a3e53f
Nguyen Duc Huy <nxhuy@zonemail.clpccd.edu>
2020-12-05 16:07:47 +0700

    bỏ phần thừa ở dưới

--- app/src/main/java/application/tool/activity/message/adapter/MessageAdapter.java
+++ app/src/main/java/application/tool/activity/message/adapter/MessageAdapter.java
@@ -53,10 +53,10 @@ public class MessageAdapter extends ArrayAdapter<MessageForConversation> {
         ImageView avatar = convertView.findViewById(R.id.avatarTo);/////////////////////////  avatar to
         CardView layout = convertView.findViewById(R.id.layout);
         //////////////
-        if(new PositionTo().checkPosition(position,user.getEmail(),list)){
+        if (new PositionTo().checkPosition(position, user.getEmail(), list)) {
             layout.setVisibility(View.VISIBLE);
-            new Avatar().getIconImage(list.get(position).getFrom(),avatar);
-        }else {
+            new Avatar().getIconImage(list.get(position).getFrom(), avatar);
+        } else {
             layout.setVisibility(View.INVISIBLE);
         }
         if (list.get(position).getType() == 2) {
@@ -170,12 +170,6 @@ public class MessageAdapter extends ArrayAdapter<MessageForConversation> {
                 new Avatar().getMessageImage(list.get(position).getBody(), imageTo);
                 layoutSend.setVisibility(View.GONE);
             }
-        }
-        if(new PositionTo().checkPosition(position,user.getEmail(),list)){
-            layout.setVisibility(View.VISIBLE);
-            new Avatar(list.get(position).getFrom()).setAvatar(avatar);
-        }else {
-            layout.setVisibility(View.INVISIBLE);
         }
         return convertView;
     }
