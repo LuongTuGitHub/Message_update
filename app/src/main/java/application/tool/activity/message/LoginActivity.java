afb07d51fd083938ad67d3249b25dfa46a3c6a92
LuongTuGitHub <luongvantu_t64@hus.edu.vn>
2020-12-05 18:21:43 +0700

    thieu ty intent :))))

--- app/src/main/java/application/tool/activity/message/LoginActivity.java
+++ app/src/main/java/application/tool/activity/message/LoginActivity.java
@@ -2,6 +2,7 @@ package application.tool.activity.message;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,6 +32,8 @@ public class LoginActivity extends AppCompatActivity {
                     auth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
                             Toast.makeText(LoginActivity.this, R.string.success, Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this,ContentActivity.class);
+                            startActivity(intent);
                         }
                     });
                 }
