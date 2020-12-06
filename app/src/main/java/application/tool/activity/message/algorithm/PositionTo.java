a8d39ca432e7dbef92a64685dadadb0ca246f8ee
Nguyễn Đức Huy <nxhuy@zonemail.clpccd.edu>
2020-12-06 22:12:39 +0700

    push --force làm mất hết commit của tao rồi

--- app/src/main/java/application/tool/activity/message/algorithm/PositionTo.java
+++ app/src/main/java/application/tool/activity/message/algorithm/PositionTo.java
@@ -7,39 +7,31 @@ import application.tool.activity.message.object.MessageForConversation;
 public class PositionTo {
     public boolean checkPosition(int position, String user, ArrayList<MessageForConversation> messages) {
         int length = messages.size();
-        if (!messages.get(position).getFrom().equals(user)) {
-            if (position <= length - 1) {
-                if (position >= 0) {
-                    if(position>0){
-                        if(position==length-2){
-                            if(messages.get(position).getFrom().equals(messages.get(position+1).getFrom())){
-                                return false;
-                            }
-                        }
-                        int index;
-                        for (index = position; index < length - 2; index++) {
-                            if (!messages.get(index).getFrom().equals(messages.get(index + 1).getFrom())) {
-                                break;
-                            }
-                        }
-                        for (; index >= position; index--) {
-                            if (type(messages.get(index).getType())) {
-                                break;
-                            }
-                        }
-                        return index == position;
-                    }
-                    if(length==1){
-                        return type(messages.get(position).getType());
-                    }else {
-                        if(messages.get(position+1).getFrom().equals(user)){
-                            return type(messages.get(position).getType());
-                        }else {
-                            return false;
-                        }
+        if (!messages.get(position).getFrom().equals(user) && position <= length - 1 && position >= 0) {
+            if (position > 0) {
+                if (position == length - 2 && messages.get(position).getFrom().equals(messages.get(position + 1).getFrom())) {
+                    return false;
+                }
+                int index;
+                for (index = position; index < length - 2; index++) {
+                    if (!messages.get(index).getFrom().equals(messages.get(index + 1).getFrom())) {
+                        break;
                     }
                 }
+                for (; index >= position; index--) {
+                    if (type(messages.get(index).getType())) {
+                        break;
+                    }
+                }
+                return index == position;
             }
+            if (length == 1) {
+                return type(messages.get(position).getType());
+            }
+            if (messages.get(position + 1).getFrom().equals(user)) {
+                return type(messages.get(position).getType());
+            }
+            return false;
         }
         return false;
     }
