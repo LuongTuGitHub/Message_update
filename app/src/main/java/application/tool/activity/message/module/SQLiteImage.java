f3653aea95ab2545aebccaf21f21e2a3ff021c5d
Nguyễn Đức Huy <nxhuy@zonemail.clpccd.edu>
2020-12-16 22:54:03 +0700

    1. Quên lệnh 2. đéo biết 2 là gì 3. đéo biết sai ở đâu 4. di ngủ

--- app/src/main/java/application/tool/activity/message/module/SQLiteImage.java
+++ app/src/main/java/application/tool/activity/message/module/SQLiteImage.java
@@ -21,48 +21,65 @@ public class SQLiteImage extends SQLiteOpenHelper {
     private final static String DB_IMAGE = "image.db";
     private final static int VERSION = 1;
     Context context;
+
     public SQLiteImage(@Nullable Context context) {
-        super(context, DB_IMAGE,null, VERSION);
+        super(context, DB_IMAGE, null, VERSION);
         this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-            db.execSQL("CREATE TABLE IF NOT EXISTS IMAGE(ID INTEGER PRIMARY KEY AUTOINCREMENT,UUID VARCHAR(255),BYTES BLOG)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS IMAGE(ID INTEGER PRIMARY KEY AUTOINCREMENT,UUID VARCHAR(255),BYTES BLOG)");
     }
-    public void Add(String key,byte[] bytes){
+
+    public void Add(String key, byte[] bytes) {
         String sql = "INSERT INTO IMAGE VALUES(null,?,?)";
         SQLiteStatement statement = getWritableDatabase().compileStatement(sql);
-        statement.bindString(1,key);
-        statement.bindBlob(2,bytes);
+        statement.bindString(1, key);
+        statement.bindBlob(2, bytes);
         statement.executeInsert();
     }
-    public byte[] getImage(String key){
-        byte[] bytes;
+
+    public byte[] getImage(String key) {
+        byte[] bytes = new byte[0];
         SQLiteDatabase database = getReadableDatabase();
-        @SuppressLint("Recycle") Cursor cursor = database.rawQuery("SELECT BYTES FROM IMAGE WHERE UUID LIKE ?",new String[]{key});
+        @SuppressLint("Recycle")
+        Cursor cursor = database
+                .rawQuery("SELECT UUID,BYTES FROM IMAGE WHERE UUID LIKE ?",
+                        new String[]{key});
         cursor.moveToFirst();
-        bytes = cursor.getBlob(0);
-        if (bytes==null){
+        bytes = cursor.getBlob(1);
+        if (bytes == null) {
             Resources res = context.getResources();
             @SuppressLint("UseCompatLoadingForDrawables") Drawable d = res.getDrawable(R.drawable.image_placeholder);
-            Bitmap bitmap = ((BitmapDrawable)d).getBitmap();
+            Bitmap bitmap = ((BitmapDrawable) d).getBitmap();
             ByteArrayOutputStream stream = new ByteArrayOutputStream();
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
             bytes = stream.toByteArray();
         }
         return bytes;
     }
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
-    public boolean checkExist(String key){
+
+    public boolean checkExist(String key) {
         SQLiteDatabase database = getReadableDatabase();
-        @SuppressLint("Recycle") Cursor cursor = database.rawQuery("SELECT * FROM IMAGE WHERE UUID LIKE ?",new String[]{key});
-        if(cursor.getCount()>0){
-            return true;
+        Cursor c = database.rawQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='{IMAGE}'", null);
+        if(c.getCount()>0){
+            @SuppressLint("Recycle") Cursor cursor = database.rawQuery("SELECT UUID FROM IMAGE WHERE UUID LIKE ?", new String[]{key});
+            cursor.moveToFirst();
+            if (cursor.getCount() > 0) {
+                return true;
+            }
+            return false;
+        }else {
+            SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+            sqLiteDatabase.execSQL("DROP TABLE  IF EXISTS IMAGE");
+            sqLiteDatabase.execSQL("CREATE TABLE IMAGE(ID INTEGER PRIMARY KEY AUTOINCREMENT,UUID VARCHAR(255),BYTES BLOG)");
         }
-        return false;
+        return  false; //didjt mej ngu a t quen lenh :)))) ok chwa
     }
 }
